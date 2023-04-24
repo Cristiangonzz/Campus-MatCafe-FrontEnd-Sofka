1eed6a735d920a06cb99e2a441c64b1e03394eb6
Julian362 <Juliangarcia362@hotmail.com>
2023-04-24 13:31:45 -0500

    add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './presentation/admin/admin.module';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { CourseComponent } from './presentation/course/course/course.component';
+import { CourseComponent } from './presentation/course/course.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CourseComponent
-  ],
+  declarations: [AppComponent, CourseComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     AdminModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
