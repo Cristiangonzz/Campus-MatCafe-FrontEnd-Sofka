4cbbba90a52070d20bdb0144dae4a9184be14b47
Julian362 <Juliangarcia362@hotmail.com>
2023-04-27 22:35:24 -0500

    add login

--- src/app/presentation/admin/routing-admin.module.ts
+++ src/app/presentation/admin/routing-admin.module.ts
@@ -1,27 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateUserComponent } from './create-user/create-user.component';
-import { PermissionGuard } from '../shared/guards/permission.guard';
-import { SendCalificationComponent } from './send-calification/send-calification.component';
 import { PermissionRolGuard } from '../shared/guards/permission-rol.guard';
+import { PermissionGuard } from '../shared/guards/permission.guard';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { SendCalificationComponent } from './send-calification/send-calification.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
-<<<<<<< HEAD
       {
         path: `create`,
         component: CreateUserComponent,
         canActivate: [PermissionGuard, PermissionRolGuard],
       },
-=======
-      { path: `create`, component: CreateUserComponent,canActivate: [ PermissionGuard ], },
-      { path: `send`, component: SendCalificationComponent,canActivate: [ PermissionGuard ], },
+      {
+        path: `create`,
+        component: CreateUserComponent,
+        canActivate: [PermissionGuard],
+      },
+      {
+        path: `send`,
+        component: SendCalificationComponent,
+        canActivate: [PermissionGuard],
+      },
 
->>>>>>> d51a6f7f6d6a7ebe568a7fddd03fdf3131b619e5
       { path: `**`, redirectTo: 'create' },
-
     ],
   },
 ];
