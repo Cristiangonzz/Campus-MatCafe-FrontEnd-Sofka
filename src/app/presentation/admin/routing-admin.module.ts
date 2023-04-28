92194e35d733234dd95b879ffe1b57d81544fa25
Franco Torres <104739161+Kaitach@users.noreply.github.com>
2023-04-27 23:23:04 -0300

    cambios

--- src/app/presentation/admin/routing-admin.module.ts
+++ src/app/presentation/admin/routing-admin.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { PermissionGuard } from '../shared/guards/permission.guard';
+import { SendCalificationComponent } from './send-calification/send-calification.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
       { path: `create`, component: CreateUserComponent,canActivate: [ PermissionGuard ], },
+      { path: `send`, component: SendCalificationComponent,canActivate: [ PermissionGuard ], },
       { path: `**`, redirectTo: 'create' },
+
     ],
   },
 ];
